ef19343a4a91ac3bb175f814f24ec7c91826257b
Jyguy <jskekkx@gmail.com>
2019-03-28 18:55:45 -0400

    fix +close

--- commands/close.js
+++ commands/close.js
@@ -1,5 +1,5 @@
 module.exports.run = (client, message, args) => {
-  if (!message.channel.name.startsWith('ticket') || message.parentID !== client.config.ticketcategory) return message.reply(':x: You may only use this command in ticket channels!');
+  if (!message.channel.name.startsWith('ticket') || message.channel.parentID !== client.config.ticketcategory) return message.reply(':x: You may only use this command in ticket channels!');
 
   const category = message.guild.channels.get(client.config.ticketcategory);
   if (!category) return message.reply(':x: I did not find the ticket category.');
