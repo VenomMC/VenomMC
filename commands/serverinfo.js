e189f86060e173599ba5b0e67d3e927871ac8338
Jyguy <jskekkx@gmail.com>
2019-03-30 00:39:35 -0400

    Add server online status in +serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,12 +3,13 @@ const fetch = require('node-fetch');
 module.exports.run = async (client, message, args) => {
   if (!message.channel.permissionsFor(client.user).has('EMBED_LINKS')) return message.reply(':x: I do not have the required permission `Embed Links` in this channel.');
   const body = await fetch('https://mcapi.us/server/status?ip=play.venommc.net').then(res => res.json());
-  if (body.status !== 'success' || !body.online) return message.reply(':x: Cannot retrieve server information at the time. Issue reoccurring? Contact Spiget.');
+  if (body.status !== 'success') return message.reply(':x: Cannot retrieve server information at the time. Issue reoccurring? Contact Spiget.');
 
   const embed = new client.Discord.MessageEmbed()
     .setTitle('Server Information')
-    .addField('Total Members Online', body.players.now)
-    .setColor(0x00FF00)
+    .addField('Total Members Online', body.online ? body.players.now : 'N/A')
+    .addField('Server Status', body.online ? 'Online' : 'Offline')
+    .setColor(body.online ? 0x00FF00 : 0xFF0000)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
 
   return message.channel.send(embed);
