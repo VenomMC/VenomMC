871acb51352298f317d8e16c776f0b5f7700d790
Jyguy <jskekkx@gmail.com>
2019-05-09 16:57:58 -0400

    Changed the activity.

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,16 @@
 module.exports.run = (client) => {
   return client.on('ready', () => {
     console.log(`Successfully signed in as ${client.user.tag}.`);
-    return client.user.setActivity(`${client.guilds.size} Servers`);
+
+    const activities = [
+      'you',
+      '<users> Users'
+    ];
+    let step = 0;
+    setInterval(() => {
+      if (step === activities.length) step === 0;
+      client.user.setActivity(activities[step].replace('<users>', client.users.size), { type: 'WATCHING' });
+      step += 1;
+    }, 1000 * 60 * 10);
   });
 };
