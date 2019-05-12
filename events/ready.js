e562d7543c947b11cb80a2fda21a287495a12b42
Jyguy <jskekkx@gmail.com>
2019-05-12 14:36:45 -0400

    Added more status messages.

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,22 @@ module.exports.run = client => {
 
     client.timeout = setTimeout(() => client.user.setStatus('idle'), 1000 * 60 * 5);
 
-    const activities = [
-      'you',
-      '<users> Users'
-    ];
+    const activities = {
+      'you': 'WATCHING',
+      '<users> Users': 'WATCHING',
+      'Jyguy at School': 'WATCHING',
+      'Cat Videos': 'WATCHING',
+      'People': 'WATCHING',
+      'Anime': 'WATCHING',
+      'my screen': 'WATCHING',
+      'with my cats': 'PLAYING',
+      'being AFK': 'PLAYING'
+    };
     let step = 1;
-    client.user.setActivity(activities[0], { type: 'WATCHING' });
+    client.user.setActivity(Object.keys(activities)[0], { type: Object.values(activities)[0] });
     setInterval(() => {
-      client.user.setActivity(activities[step].replace('<users>', client.users.size), { type: 'WATCHING' });
+      const name = Object.keys(activities)[step];
+      client.user.setActivity(name.replace('<users>', client.users.size), { type: activities[name] });
       if (step === activities.length - 1) step = 0;
       else step += 1;
     }, 1000 * 60 * 10);
