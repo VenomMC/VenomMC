09c0587707019d138e6f1bb50c9fb79236608823
Jyguy <jskekkx@gmail.com>
2019-05-15 21:48:57 -0400

    Added a leaving message.

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,15 @@
+function sendMsg(client, member) {
+  const channel = member.guild.channels.find(c => c.name === 'welcome-goodbye');
+  if (!channel) return;
+  if (!channel.permissionsFor(member.guild.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) return;
+
+  channel.send(`:negative_squared_cross_mark: ${member} left **${client.escMD(member.guild.name)}**`);
+}
+
+module.exports.run = client => {
+  return client.on('guildMemberAdd', member => {
+    if (!member.guild.available) return;
+
+    sendMsg(client, member);
+  });
+};
