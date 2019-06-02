54c5c3cd6c6d46a88cf572c95ead839220186c27
Jyguy <jskekkx@gmail.com>
2019-06-02 00:35:18 -0400

    Removed the message interval in +ping.

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,5 @@
 module.exports.run = async (client, message, args) => {
-  const msg = await message.channel.send(`Pong! :heartbeat: \`${Math.round(client.ws.ping)}ms\``);
-  return msg.edit(`${msg.content} :stopwatch: \`${Date.now() - msg.createdTimestamp}ms\``);
+  return message.channel.send(`Pong! :heartbeat: \`${Math.round(client.ws.ping)}ms\``);
 };
 
 module.exports.help = {
