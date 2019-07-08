dc3dd89ca0778e99575ad5c542d5c2a1152d9b86
Jyguy <jskekkx@gmail.com>
2019-07-08 19:35:37 -0400

    Fixed all channels being checked for LOA.

--- functions/checkLOA.js
+++ functions/checkLOA.js
@@ -1,4 +1,5 @@
 module.exports.run = message => {
+  if (message.channel.name !== 'staff-loa') return;
   if (!message.channel.permissionsFor(message.guild.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) return;
   if (!message.content.includes('Reason: I don\'t have time')) return;
 
