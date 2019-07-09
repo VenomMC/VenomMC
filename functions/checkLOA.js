b94718b110c09996d3c1cfc16c7ec5cc74aa25bc
Jyguy <jskekkx@gmail.com>
2019-07-08 20:12:07 -0400

    im tired ok

--- functions/checkLOA.js
+++ functions/checkLOA.js
@@ -1,8 +1,9 @@
 module.exports.run = message => {
   if (message.channel.name !== 'staff-loa') return;
   if (!message.channel.permissionsFor(message.guild.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) return;
-  if (!message.content.includes('Reason: ') || message.content.includes('Reason: I don\'t have time')) return;
+  if (!message.content.includes('Reason: ')) return;
+  if (message.content.includes('Reason: I don\'t have time')) return message.reply('Declined :x:');
 
-  message.guild.channels.find('management-loa-logs').send(message.content);
+  message.guild.channels.find(c => c.name === 'management-loa-logs' && c.type === 'text').send(message.content);
   return message.reply('Accepted :white_check_mark:');
 };
