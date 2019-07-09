b1294ad8d78941361cb076cd18aad6b8281ba85e
Jyguy <jskekkx@gmail.com>
2019-07-08 20:08:27 -0400

    my brain cells are dying

--- functions/checkLOA.js
+++ functions/checkLOA.js
@@ -1,7 +1,7 @@
 module.exports.run = message => {
   if (message.channel.name !== 'staff-loa') return;
   if (!message.channel.permissionsFor(message.guild.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) return;
-  if (message.content.includes('Reason: I don\'t have time')) return;
+  if (!message.content.includes('Reason: ') || message.content.includes('Reason: I don\'t have time')) return;
 
   return message.reply('Accepted :white_check_mark:');
 };
