7d312d95b0197331aea83a859acbe373c4ac003b
Jyguy <jskekkx@gmail.com>
2019-07-08 20:10:18 -0400

    Added logs for staff LOA.

--- functions/checkLOA.js
+++ functions/checkLOA.js
@@ -3,5 +3,6 @@ module.exports.run = message => {
   if (!message.channel.permissionsFor(message.guild.me).has(['VIEW_CHANNEL', 'SEND_MESSAGES'])) return;
   if (!message.content.includes('Reason: ') || message.content.includes('Reason: I don\'t have time')) return;
 
+  message.guild.channels.find('management-loa-logs').send(message.content);
   return message.reply('Accepted :white_check_mark:');
 };
