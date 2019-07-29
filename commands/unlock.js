9e054d9fb3500095365746c1770102822c3d5da9
Jyguy <jskekkx@gmail.com>
2019-07-29 12:15:02 -0400

    Added +lock and +unlock.

---
+++ commands/unlock.js
@@ -0,0 +1,19 @@
+module.exports.run = (client, message, args) => {
+  if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(':x: You require the permission `Administrator`.');
+
+  const channels = message.guild.channels.filter(c => client.config.staffchannels.includes(c.id));
+  if (channels.some(c => !c)) return message.reply(':x: 1 or more of the staff channels has gone missing. Please tell Spiget to recalibrate the IDs.');
+
+  channels.forEach(c => c.overwritePermissions({
+    permissionOverwrites: {
+      id: '592498342307823616',
+      allow: ['SEND_MESSAGES']
+    }
+  }));
+  return message.channel.send('Successfully unlocked all the staff channels.');
+};
+
+module.exports.help = {
+  desc: 'Unlocks all the staff channels.',
+  usage: 'unlock'
+};
