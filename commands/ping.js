7c53d4d59b1f2b96540867a35c77fbf732a65d36
Jyguy <jskekkx@gmail.com>
2019-08-20 01:39:37 -0400

    Sleep is healthy kids

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
   const msg = await message.channel.send(`Pong! :heartbeat: \`${Math.round(client.ws.ping)}ms\``);
-  return msg.edit(`${msg.content} :stopwatch: \`${Date.now() - msg.createdTimestamp()}ms\``);
+  return msg.edit(`${msg.content} :stopwatch: \`${Date.now() - msg.createdTimestamp}ms\``);
 };
 
 module.exports.help = {
