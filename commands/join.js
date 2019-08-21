d86309a6b0b118327908fb06890d43ba5620ae1f
Jyguy <jskekkx@gmail.com>
2019-08-20 22:03:09 -0400

    Added +join and +leave.

---
+++ commands/join.js
@@ -0,0 +1,13 @@
+module.exports.run = (client, message, args) => {
+  if (!message.member.voice.channel) return message.reply(':x: You must be in a voice channel to run this command.');
+  const vc = message.member.voice.channel;
+  if (!vc.joinable) return message.reply(':x: I am missing permissions to join your voice channel or the voice channel is full.');
+
+  vc.join();
+  return message.channel.send('Successfully joined your voice channel.');
+};
+
+module.exports.desc = {
+  desc: 'Joins your voice channel.',
+  usage: 'join'
+};
---
+++ commands/leave.js
@@ -0,0 +1,13 @@
+module.exports.run = async (client, message, args) => {
+  const music = client.functions.get('music');
+  if (music.connection && music.connection.channel.id !== message.member.voice.channelID) return message.reply(':x: You must be in the same voice channel as me to use this command.');
+
+  if (music.connection) music.connection.channel.leave();
+  music.endSession();
+  return message.channel.send('Successfully left the voice channel.');
+};
+
+module.exports.help = {
+  desc: 'Leaves the voice channel the bot is in. This could be seen as a force music stop.',
+  usage: 'leave'
+};
