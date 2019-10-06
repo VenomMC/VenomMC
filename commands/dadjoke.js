4e6df5c2869e36dafebfe2c3a02066c69d58831c
Jyguy <jskekkx@gmail.com>
2019-10-06 13:19:43 -0400

    added +dadjoke

---
+++ commands/dadjoke.js
@@ -0,0 +1,10 @@
+module.exports.run = async (client, message, args) => {
+  const json = await client.fetch('https://icanhazdadjoke.com/', { headers: { Accept: 'application/json' } }).then(res => res.json());
+  if (!json.joke) return message.reply(':x: The dad joke API seems to be down.');
+  return message.channel.send(json.joke);
+};
+
+module.exports.help = {
+  desc: 'Tells you a dad joke.',
+  usage: 'dadjoke'
+};
