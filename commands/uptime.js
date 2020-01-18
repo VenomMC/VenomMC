600b24f5c225df73180b0aa26ae6df2e36212e3c
Jyguy <jskekkx@gmail.com>
2020-01-18 14:08:03 -0500

    add formatTime function

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,10 +1,5 @@
 module.exports.run = (client, message, args) => {
-  const seconds = Math.floor(client.uptime / 1000 % 60);
-  const minutes = Math.floor(client.uptime / 1000 / 60 % 60);
-  const hours = Math.floor(client.uptime / 1000 / 60 / 60 % 24);
-  const days = Math.floor(client.uptime / 1000 / 60 / 60 / 24);
-
-  return message.channel.send(`The bot has been online for: **${days}** Days, **${hours}** Hours, **${minutes}** Minutes, and **${seconds}** Seconds.`);
+  return message.channel.send(`The bot has been online for: **${client.functions.formatTime(client.uptime)}**`);
 };
 
 module.exports.help = {
