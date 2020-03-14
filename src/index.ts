9c2924e2f5f0dad19e3afe9679a447738c89cb84
Jyguy <jskekkx@gmail.com>
2020-03-14 16:52:22 -0400

    fix(CommandManager): filter breaks due to setting values in constructor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires, global-require */
 
+import { Command } from 'VenomBot';
 import { VenomClient } from './structures/Client';
 import { config } from 'dotenv';
 import { readdirSync } from 'fs';
@@ -9,6 +10,15 @@ config();
 const client = new VenomClient({
   disableMentions: 'everyone'
 });
+
+const categories = readdirSync('./dist/commands');
+categories.forEach(c => readdirSync(`./dist/commands/${c}`).forEach(cmd => {
+  const command: Command = require(`../commands/${c}/${cmd}`);
+  client.commands.set(cmd.slice(0, -3), command);
+
+  if (!Object.values(client.commands.categories).includes(command.help.category)) client.commands.categories[c] = command.help.category;
+}));
+
 readdirSync('./dist/events').forEach(event => client.on(event.slice(0, -3), (...args) => require(`./events/${event}`).run(client, ...args)));
 
 client.login(process.env.BOT_TOKEN);
--- src/structures/CommandManager.ts
+++ src/structures/CommandManager.ts
@@ -1,20 +1,7 @@
 import { Collection } from 'discord.js';
 import { Command } from 'VenomBot';
-import { readdirSync } from 'fs';
 
 export class CommandManager extends Collection<string, Command> {
-  public constructor () {
-    super();
-
-    const categories = readdirSync('./dist/commands');
-    categories.forEach(c => readdirSync(`./dist/commands/${c}`).forEach(cmd => {
-      const command: Command = require(`../commands/${c}/${cmd}`); // eslint-disable-line @typescript-eslint/no-var-requires, global-require
-      this.set(cmd.slice(0, -3), command);
-
-      if (!Object.values(this.categories).includes(command.help.category)) this.categories[c] = command.help.category;
-    }));
-  }
-
   public categories: { [ rawCategory: string ]: string } = {};
 
   public checkAlias (cmd: string) {
