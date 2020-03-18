e75ab727d5f579e3e5ce853d3580dc4ff53c4d6e
Jyguy <jskekkx@gmail.com>
2020-03-17 22:33:40 -0400

    added +add

---
+++ src/commands/staff/add.ts
@@ -0,0 +1,37 @@
+import { PermissionString } from 'discord.js';
+import { VenomClient } from '../../structures/Client';
+import { GuildMessage, HelpObj } from 'VenomBot';
+
+export async function run (client: VenomClient, message: GuildMessage, args: string[]) {
+  if (!message.member.roles.cache.some(r => r.name === 'Management')) return message.reply('You must be part of Management to use this command.');
+
+  if (!args[1]) return message.reply('You must provide a member to give the Pending status to.');
+  const member = await message.guild.members.fetch(args[1].replace(/[<>@!?]/g, '')).catch(() => null);
+  if (!member) return message.reply('That member was invalid.');
+  if (member.roles.cache.has(client.config.staffrole)) return message.reply('That member is already a staff member.');
+
+  const pending = message.guild.roles.cache.find(r => r.name === 'Pending');
+  if (!pending) return message.reply('I did not find the role "Pending".');
+  if (message.guild.me!.roles.highest.comparePositionTo(pending) <= 0) return message.reply('My role position is not high enough to assign the Pending role.');
+
+  const { rows } = await client.query('SELECT * FROM pending WHERE userid = $1', [ member.id ]);
+  if (rows.length === 1) return message.reply('That member is already Pending.');
+
+  member.roles.add(pending);
+  client.query('INSERT INTO pending (userid) VALUES ($1)', [ member.id ]);
+  message.channel.send(`Successfully set \`\`${client.escInline(member.user.tag)}\`\`'s status to Pending.`);
+}
+
+export const help: HelpObj = {
+  aliases: [],
+  category: 'Staff',
+  desc: 'Adds a member as "Pending" and removes the role in the next Sunday at midnight UTC. Once this period is over, the user will receive the Helper and Staff roles. This can be cancelled mid-way with +remove.',
+  usage: 'add <Member>',
+  venom: true
+};
+
+export const clientPerms: PermissionString[] = [
+  'MANAGE_ROLES'
+];
+
+export const memberPerms: PermissionString[] = [];
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,6 @@
 import { VenomClient } from '../structures/Client';
 import fetch from 'node-fetch';
+import { schedule } from 'node-cron';
 
 async function setActivity (client: VenomClient) {
   let name = null;
@@ -15,8 +16,44 @@ async function setActivity (client: VenomClient) {
   }, 1000 * 60 * 10);
 }
 
+function setCron (client: VenomClient) {
+  const guild = client.guilds.cache.get(client.config.officialserver);
+  if (!guild) return;
+
+  schedule('0 0 * * Sunday', async () => {
+    const { rows } = await client.query('SELECT * FROM pending');
+    client.query('DELETE FROM pending');
+
+    if (!guild.me!.hasPermission('MANAGE_ROLES')) return;
+
+    const pending = guild.roles.cache.find(r => r.name === 'Pending');
+    if (!pending) return;
+    if (guild.me!.roles.highest.comparePositionTo(pending) <= 0) return;
+
+    const staff = guild.roles.cache.get(client.config.staffrole);
+    if (!staff) return;
+    if (guild.me!.roles.highest.comparePositionTo(staff) <= 0) return;
+
+    const helper = guild.roles.cache.find(r => r.name === 'Staff');
+    if (!helper) return;
+    if (guild.me!.roles.highest.comparePositionTo(helper) <= 0) return;
+
+    for (const row of rows) {
+      const member = await guild.members.fetch(row.userid).catch(() => null);
+      if (!member) return;
+      member.setNickname(`Helper | ${member.displayName}`);
+      if (member.roles.cache.has(pending.id)) member.roles.remove(pending);
+      if (!member.roles.cache.has(staff.id)) member.roles.add(staff);
+      if (!member.roles.cache.has(helper.id)) member.roles.add(helper);
+    }
+  }, {
+    timezone: 'America/New_York'
+  });
+}
+
 export async function run (client: VenomClient) {
   console.log(`Successfully signed in as ${client.user!.tag}. (ID ${client.user!.id})`);
 
   setActivity(client);
+  setCron(client);
 }
